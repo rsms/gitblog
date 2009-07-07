6c866e6793f9a1eb0dfa76cd0a27de813007d555
Rasmus Andersson <rasmus@flajm.com>
2009-07-08 00:44:45 +0200

    feed updates

--- themes/default/feed.php
+++ themes/default/feed.php
@@ -25,20 +25,17 @@ echo '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL;
 		<published><?= date('c', $post->published) ?></published>
 		<updated><?= date('c', $post->modified) ?></updated>
 		<?= $post->tagLinks('<category scheme="'.GB_SITE_URL.gb::$tags_prefix.'" term="%n" />',
-			"\n\t\t", "\n\t\t") ?>
+			"\n\t\t", "\n\t\t").($post->tags ? "\n\t\t" : '') ?>
 		<?= $post->categoryLinks('<category scheme="'.GB_SITE_URL.gb::$categories_prefix.'" term="%n" />',
-			"\n\t\t", "\n\t\t") ?>
+			"\n\t\t", "\n\t\t").($post->categories ? "\n\t\t" : '') ?>
 		<comments><?= $post->comments ?></comments>
 		<gb:version><?= $post->id ?></gb:version>
 		<? if ($post->excerpt): ?>
 		<summary type="html"><![CDATA[<?= $post->excerpt ?>]]></summary>
 		<? endif ?>
-		<content type="html" xml:base="<?= $post->url() ?>"><![CDATA[
-			<?= $post->body ?>
-			<? if ($post->excerpt): ?>
-				<p><a href="<?= $post->url() ?>#<?= $post->domID() ?>-more">Read more...</a></p>
-			<? endif; ?>
-		]]></content>
+		<content type="html" xml:base="<?= $post->url() ?>"><![CDATA[<?= $post->body ?><? if ($post->excerpt): ?>
+			<p><a href="<?= $post->url() ?>#<?= $post->domID() ?>-more">Read more...</a></p>
+		<? endif; ?>]]></content>
 		<link rel="replies" type="text/html" href="<?= $post->url() ?>#comments" thr:count="<?= $post->comments ?>" />
 		<link rel="replies" type="application/atom+xml" href="<?= GB_SITE_URL.gb::$feed_prefix ?>" thr:count="<?= $post->comments ?>" />
 		<thr:total><?= $post->comments ?></thr:total>

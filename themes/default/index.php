d8e44f0170eaa1575008663afd0e26b8046bfbc7
Rasmus Andersson <rasmus@notion.se>
2009-07-12 14:45:12 +0200

    flushing after header and fixed incorrect content type meta header

--- themes/default/index.php
+++ themes/default/index.php
@@ -14,10 +14,11 @@ if (gb::$is_404)
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8"/>
 		<title><?= gb_title() ?></title>
 		<link href="<?= GB_THEME_URL ?>style.css" type="text/css" rel="stylesheet" media="screen" />
 	</head>
+	<? flush() ?>
 	<body>
 		<?
 		

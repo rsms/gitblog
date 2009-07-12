a3e4bd01e55c67263566d84325eb5ac576de15c1
Rasmus Andersson <rasmus@notion.se>
2009-07-12 14:44:12 +0200

    everything is escaped and safe

--- helpers/feed.php
+++ helpers/feed.php
@@ -1,43 +1,45 @@
 <?
+$updated_time = $postspage->posts ? $postspage->posts[0]->modified->time : time();
+
 header('Content-Type: application/atom+xml; charset=utf-8');
-header('Last-Modified: '.date('r', $postspage->posts ? $postspage->posts[0]->modified : time()));
+header('Last-Modified: '.date('r', $updated_time));
 echo '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL;
 ?>
 <feed xmlns="http://www.w3.org/2005/Atom" 
       xmlns:thr="http://purl.org/syndication/thread/1.0"
       xmlns:gb="http://gitblog.se/ns/atom/1.0"
       xml:lang="en"
-      xml:base="<?= GB_SITE_URL.gb::$feed_prefix ?>">
-	<id><?= GB_SITE_URL.gb::$feed_prefix ?></id>
-	<title><?= gb::$site_title ?></title>
-	<link rel="alternate" type="text/html" href="<?= GB_SITE_URL ?>" />
-	<updated><?= date('c', $postspage->posts ? $postspage->posts[0]->modified : time()) ?></updated>
+      xml:base="<?= h(GB_SITE_URL) ?>">
+	<id><?= h(gb::url()) ?></id>
+	<title><?= h(gb::$site_title) ?></title>
+	<link rel="alternate" type="text/html" href="<?= h(GB_SITE_URL) ?>" />
+	<updated><?= date('c', $updated_time) ?></updated>
 	<generator uri="http://gitblog.se/" version="<?= GB_VERSION ?>">Gitblog</generator>
 <? foreach ($postspage->posts as $post): ?>
 	<entry>
-		<title type="html"><?= $post->title ?></title>
+		<title type="html"><?= h($post->title) ?></title>
 		<author>
-			<name><?= $post->author->name ?></name>
-			<uri><?= GB_SITE_URL ?></uri>
+			<name><?= h($post->author->name) ?></name>
+			<uri><?= h(GB_SITE_URL) ?></uri>
 		</author>
-		<link rel="alternate" type="text/html" href="<?= $post->url() ?>" />
-		<id><?= $post->url() ?></id>
-		<published><?= date('c', $post->published) ?></published>
-		<updated><?= date('c', $post->modified) ?></updated>
-		<?= $post->tagLinks('<category scheme="'.GB_SITE_URL.gb::$tags_prefix.'" term="%n" />',
+		<link rel="alternate" type="text/html" href="<?= h($post->url()) ?>" />
+		<id><?= h($post->url()) ?></id>
+		<published><?= $post->published ?></published>
+		<updated><?= $post->modified ?></updated>
+		<?= $post->tagLinks('<category scheme="'.gb::url_to('tags').'" term="%n" />',
 			"\n\t\t", "\n\t\t").($post->tags ? "\n" : '') ?>
-		<?= $post->categoryLinks('<category scheme="'.GB_SITE_URL.gb::$categories_prefix.'" term="%n" />',
+		<?= $post->categoryLinks('<category scheme="'.gb::url_to('categories').'" term="%n" />',
 			"\n\t\t", "\n\t\t").($post->categories ? "\n" : '') ?>
 		<comments><?= $post->comments ?></comments>
 		<gb:version><?= $post->id ?></gb:version>
 		<? if ($post->excerpt): ?>
 		<summary type="html"><![CDATA[<?= $post->excerpt ?>]]></summary>
 		<? endif ?>
-		<content type="html" xml:base="<?= $post->url() ?>"><![CDATA[<?= $post->body ?><? if ($post->excerpt): ?>
-			<p><a href="<?= $post->url() ?>#<?= $post->domID() ?>-more">Read more...</a></p>
+		<content type="html" xml:base="<?= h($post->url()) ?>"><![CDATA[<?= $post->body ?><? if ($post->excerpt): ?>
+			<p><a href="<?= h($post->url()) ?>#<?= $post->domID() ?>-more">Read more...</a></p>
 		<? endif; ?>]]></content>
-		<link rel="replies" type="text/html" href="<?= $post->url() ?>#comments" thr:count="<?= $post->comments ?>" />
-		<link rel="replies" type="application/atom+xml" href="<?= GB_SITE_URL.gb::$feed_prefix ?>" thr:count="<?= $post->comments ?>" />
+		<link rel="replies" type="text/html" href="<?= h($post->url()) ?>#comments" thr:count="<?= $post->comments ?>" />
+		<link rel="replies" type="application/atom+xml" href="<?= h(gb::url_to('feed')) ?>" thr:count="<?= $post->comments ?>" />
 		<thr:total><?= $post->comments ?></thr:total>
 	</entry>
 <? endforeach ?>

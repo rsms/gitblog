c1b6fb5be6b252de02f68fc07857a92adb1553ca
Rasmus Andersson <rasmus@notion.se>
2009-07-15 21:20:42 +0200

    fancy pantsy menu

--- admin/index.php
+++ admin/index.php
@@ -8,4 +8,8 @@ include '_header.php';
 <p>
 	Authenticated as <?= h(gb::$authenticated) ?>
 </p>
+<h3>Fancy menu</h3>
+<p>
+	<a href="import-wordpress.php">Import a Wordpress blog</a>
+</p>
 <? include '_footer.php' ?>

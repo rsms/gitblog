2d8a9e4fb4d85b03b821b64e01513e484e38b12a
Rasmus Andersson <rasmus@notion.se>
2009-08-04 15:40:56 +0200

    only showing comments div if there are any comments or comments are open. also, moved the comment javascript to a separate file and moved around in the sidebar

--- themes/default/post.php
+++ themes/default/post.php
@@ -21,6 +21,7 @@
 		<div class="breaker"></div>
 	</div>
 	<!-- =========================== comments =========================== -->
+	<? if ($post->commentsOpen || count($post->comments)): ?>
 	<div id="comments">
 		<hr/>
 		<div class="wrapper">
@@ -68,6 +69,7 @@
 						<div class="actions">
 							<? if (gb::$authorized): ?>
 								<a class="rm" href="<?= h($comment->removeURL()) ?>"
+									onclick="return confirm('Really delete this comment?')"
 									title="Remove this comment and hide any replies to it"><span>&otimes;</span></a>
 							<? endif ?>
 							<a class="reply" href="javascript:reply('<?= $comment->id ?>');" 
@@ -121,115 +123,13 @@
 				</p>
 				<div class="breaker"></div>
 			</form>
-			<script type="text/javascript">
-				//<![CDATA[
-				function trim(s) {
-					return s.replace(/(^[ \t\s\n\r]+|[ \t\s\n\r]+$)/g, '');
-				}
-				document.getElementById('comment-form').onsubmit = function(e) {
-					function check_filled(id, default_value) {
-						var elem = document.getElementById(id);
-						if (!elem)
-							return false;
-						elem.value = trim(elem.value);
-						if (elem.value == default_value || elem.value == '') {
-							elem.select();
-							return false;
-						}
-						return true;
-					}
-					if (!check_filled('comment-reply-message', ''))
-						return false;
-					if (!check_filled('comment-author-name', 'Name'))
-						return false;
-					if (!check_filled('comment-author-email', 'Email'))
-						return false;
-					return true;
-				}
-
-				// reply-to
-				var reply_to_comment = null;
-
-				function reply(comment_id) {
-					reply_to_comment = document.getElementById('comment-'+comment_id);
-					document.getElementById('comment-reply-to').value = comment_id;
-				}
-
-				var reply_to = document.getElementById('comment-reply-to');
-				var reply_to_lastval = "";
-				var form_parent = null;
-				var cancel_button = null;
-
-				reply_to.onchange = function(e) {
-					reply_to.value = trim(reply_to.value);
-					var title = document.getElementById('reply-title');
-					var form = document.getElementById('comment-form');
-
-					// remove any cancel button
-					if (cancel_button != null) {
-						if (cancel_button.parentNode)
-							cancel_button.parentNode.removeChild(cancel_button);
-						cancel_button = null;
-					}
-
-					if (reply_to.value != "") {
-						if (reply_to_comment == null) {
-							reply_to.value = "";
-							return;
-						}
-
-						if (form_parent == null)
-							form_parent = form.parentNode;
-
-						cancel_button = document.createElement('input');
-						cancel_button.setAttribute('type', 'button');
-						cancel_button.setAttribute('value', 'Cancel');
-						cancel_button.onclick = function(e) { document.getElementById('comment-reply-to').value = ""; };
-
-						// find submit button and append the form to its parent
-						var inputs = form.getElementsByTagName("input");
-						for (var i=0; i<inputs.length; i++) {
-							var elem = inputs.item(i);
-							if (elem.getAttribute('type') == 'submit') {
-								elem.parentNode.appendChild(cancel_button);
-								break;
-							}
-						}
-
-						form.className = "inline-reply";
-						reply_to_comment.appendChild(form);
-						title.style.display = 'none';
-
-						//document.location.hash = "comment-"+reply_to.value;
-					}
-					else {
-						form.className = "";
-						if (form_parent != null)
-							form_parent.appendChild(form);
-						title.style.display = '';
-						document.location.hash = "reply";
-					}	
-					setTimeout(function(){
-						document.getElementById('comment-reply-message').select()
-					},100);
-				}
-				setInterval(function(e){
-					if (reply_to_lastval != reply_to.value)
-						reply_to.onchange(e);
-					reply_to_lastval = reply_to.value;
-				},200);
-
-				// select message on reply
-				setTimeout(function(){if (document.location.hash == '#reply')
-					document.getElementById('comment-reply-message').select();
-				},100);
-			//]]>
-			</script>
+			<script type="text/javascript" src="<?= gb::$theme_url ?>comment.js"></script>
 		<? else: ?>
 			<p>Comments are closed.</p>
 		<? endif; ?>
 		</div>
 	</div>
+	<? endif; ?>
 	<div class="breaker"></div>
 </div>
 <? gb_flush() ?>
--- themes/default/sidebar.php
+++ themes/default/sidebar.php
@@ -1,16 +1,20 @@
 <div id="sidebar">
+	<h2>Recent comments</h2>
+	<ol class="recent-comments">
+	<? foreach (gb::index('recent-comments') as $tuple): list($comment, $_post) = $tuple; ?>
+		<li>
+			<a href="<?= h($_post->url()) ?>#comment-<?= $comment->id ?>"><?= h($comment->name) ?>
+				on <em><?= h($_post->title) ?></em></a>
+			<small><?= $comment->date->age() ?></small>
+		</li>
+	<? endforeach ?>
+	</ol>
+	
 	<h2>Popular</h2>
 	<p>Posts &amp; pages</p>
 	
 	<h2>Friends</h2>
 	<p>Who invented the weird word "blogroll"?</p>
-	
-	<h2>Recent comments</h2>
-	<ol>
-	<? foreach (gb::index('recent-comments') as $tuple): list($comment, $_post) = $tuple; ?>
-		<li><a href="<?= h($_post->url()) ?>#comment-<?= $comment->id ?>"><?= h($comment->name) ?> on <em><?= h($_post->title) ?></em></a></li>
-	<? endforeach ?>
-	</ol>
 	
 	<h2>Archive</h2>
 	<p>Some kind of calendar or maybe a list of months?</p>

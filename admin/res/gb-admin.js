8c913cf3f30dd73ab5e57e6a0124fc155b00cf4a
Rasmus Andersson <rasmus@>
2009-08-17 09:06:23 +0200

    bold and italic Liberation Serif for default theme

---
+++ admin/res/gb-admin.js
@@ -0,0 +1,52 @@
+var c = {log:function(){}};
+if (typeof console != 'undefined' && typeof console.log != 'undefined')
+	c = console;
+
+var ui = {
+	alert: function(msg) {
+		var ul = $('#gb-errors ul');
+		ul.html('');
+		ul.append('<li class="title">An error occured</li>');
+		ul.append('<li class="msg"></li>');
+		$('#gb-errors ul li.msg').text(msg);
+		$('#gb-errors').slideDown("fast");
+	},
+	
+	hideAlert: function() {
+		$('#gb-errors').slideUp("fast");
+	}
+};
+
+var http = {
+	postForm: function(kw) {
+		kw.data = http.toFormParams(kw.data);
+		return http.post(kw);
+	},
+	
+	post: function(kw) {
+		kw.type = "POST";
+		c.log(kw.type, kw.url, kw.data);
+		return $.ajax(kw);
+	},
+	
+	toFormParams: function(params) {
+		if (typeof params == 'object')
+			for (var k in params)
+				http._flattenComplexFormParams(params, k, params[k]);
+		return params;
+	},
+	
+	_flattenComplexFormParams: function(params, k, v) {
+		if (typeof v == 'object') {
+			for (var x in v) {
+				var k2 = k+'['+x+']';
+				var v2 = v[x];
+				if (!http._flattenComplexFormParams(params, k2, v2))
+					params[k2] = v2;
+			}
+			delete(params[k]);
+			return true;
+		}
+		return false;
+	}
+};

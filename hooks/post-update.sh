c609243af66e7c84bdd239a7382c09f9449fc96e
Rasmus Andersson <rasmus@notion.se>
2009-08-28 17:37:06 +0200

    support for detached secret

--- hooks/post-update.sh
+++ hooks/post-update.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-SECRET=$(grep 'gb::$secret' ../gb-config.php | cut -d' ' -f 3 | sed "s/[;']//g")
+SECRET=
+if [ -f ../secret ]; then
+	SECRET=$(cat ../secret)
+else
+	SECRET=$(grep 'gb::$secret' ../gb-config.php | cut -d' ' -f 3 | sed "s/[;']//g")
+fi
 curl \
 	-H 'X-gb-shared-secret: '$SECRET \
 	--connect-timeout 5 \

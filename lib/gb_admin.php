8d406c0bb890797738a23bc0c36c3200d781f94d
Rasmus Andersson <rasmus@notion.se>
2009-10-16 15:18:41 +0200

    todo menu items are disabled

--- lib/gb_admin.php
+++ lib/gb_admin.php
@@ -13,17 +13,17 @@ class gb_admin {
 		'h' => array('Dashboard',''),
 		array('New',null,array(
 			'n' => array('Post', 'edit/post.php'),
-			'p' => array('Page', 'edit/page.php')
+			'p' => array('Page', '#todo:edit/page.php')
 		)),
 		array('Manage',null, array(
 			array('Posts','manage/posts.php'),
-			array('Pages','manage/pages.php'),
-			array('Attachments','manage/attachments.php')
+			array('Pages','#todo:manage/pages.php'),
+			array('Attachments','#todo:manage/attachments.php')
 		)),
 		array('Settings',null, array(
 			array('Basics','settings/basics.php'),
-			array('Theme','settings/theme.php'),
-			array('Plugins','settings/plugins.php', array(
+			array('Theme','#todo:settings/theme.php'),
+			array('Plugins','#todo:settings/plugins.php', array(
 				# This is a good place for plugins to add custom menu items. Example:
 				# array('Some plugin', '../plugins/some-plugin/ui.php')
 			))
@@ -37,7 +37,7 @@ class gb_admin {
 	# resolved by render_menu
 	public static $current_domid = '';
 	
-	function render_menu($disabled=false, $items=null, $baseurl=null, $currurlpath=null, 
+	function render_menu($menu_disabled=false, $items=null, $baseurl=null, $currurlpath=null, 
 	                     $liststart='<ul>', $listend='</ul>')
 	{
 		if ($items === null)
@@ -55,33 +55,40 @@ class gb_admin {
 		$s = $liststart;
 		foreach ($items as $k => $item) {
 			$uri = $url = '';
-			$is_curr = false;
+			$is_curr = $is_todo = false;
 			$accesskey = is_string($k) ? strtoupper($k) : '';
 			if (isset($item[1]) && is_string($item[1])) {
 				$uri = $item[1];
-				$url = ($uri && $uri{0} === '/' || strpos($uri, '://') !== false) ? $uri : $baseurl . $uri;
-				$actual_currpath = GBURL::parse($url)->path;
-				$is_curr = $actual_currpath === substr($currurlpath, 0, strlen($actual_currpath));
+				$url = ($uri && ($uri{0} === '/' || strpos($uri, '://') !== false)) ? $uri : $baseurl . $uri;
+				$url_st = GBURL::parse($url);
+				$actual_currpath = $url_st->path;
+				$is_todo = strpos($url_st->fragment, 'todo:') === 0;
+				$is_curr = ( !$is_todo ) && ( $actual_currpath === substr($currurlpath, 0, strlen($actual_currpath)) );
 				if ($uri === '')
 					$is_curr = gb::url()->path === GBURL::parse(gb_admin::$url)->path;
 			}
 			$dom_id = $uri ? gb_strtodomid(gb_filenoext($item[1])) : $k;
 			$s .= '<li id="menu-item-'.$dom_id.'"';
+			$css_class = '';
 			if ($is_curr) {
-				$s .= ' class="selected"';
+				$css_class .= 'selected';
 				self::$current_domid = $dom_id;
 			}
+			if ($is_todo)
+				$css_class .= ' todo';
+			if ($css_class)
+				$s .= ' class="'.$css_class.'"';
 			$s .= '><a';
-			if ($url && !$disabled)
+			if ($url && !$is_todo && !$menu_disabled)
 				$s .= ' href="'.h($url).'"';
-			if ($accesskey && !$disabled)
+			if ($accesskey && !$menu_disabled)
 				$s .= ' accesskey="'.$accesskey.'"';
 			$s .= '><span class="title">'.h($item[0]).'</span>';
-			if ($accesskey && !$disabled)
+			if ($accesskey && !$menu_disabled)
 				$s .= '<span class="accesskey-hint">'.$accesskey_prefix.$accesskey.'</span>';
 			$s .= '</a>';
 			if (isset($item[2]) && $item[2])
-				$s .= self::render_menu($disabled, $item[2], $baseurl, $currurlpath, $liststart, $listend);
+				$s .= self::render_menu($menu_disabled, $item[2], $baseurl, $currurlpath, $liststart, $listend);
 			$s .= '</li>';
 		}
 		$s .= $listend;

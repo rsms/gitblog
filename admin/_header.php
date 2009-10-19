4ef1a20315492f25cc6c42d8423154e17c2ed3eb
Rasmus Andersson <rasmus@notion.se>
2009-10-19 09:07:55 +0200

    HTML5 FTW!

--- admin/_header.php
+++ admin/_header.php
@@ -1,12 +1,12 @@
 <?
-header('Content-Type: application/xhtml+xml; charset=utf-8');
+header('Content-Type: text/html; charset=utf-8');
 
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+?><!DOCTYPE html>
+<html lang="en">
 	<head>
-		<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8"/>
+		<meta charset="utf-8">
 		<title><?= gb_title() ?></title>
-		<link href="<?= gb_admin::$url ?>res/screen.css" type="text/css" rel="stylesheet" media="screen" />
+		<link href="<?= gb_admin::$url ?>res/screen.css" type="text/css" rel="stylesheet" media="screen">
 		<script type="text/javascript" src="<?= gb_admin::$url ?>res/jquery-1.3.2.min.js"></script>
 		<script type="text/javascript" src="<?= gb_admin::$url ?>res/jquery.json-1.3.min.js"></script>
 		<script type="text/javascript" src="<?= gb_admin::$url ?>res/gb-admin.js"></script>
